8d6471011f1f05139807ce040b74c67b176521b6
leanhduy1 <75567509+leanhduy1@users.noreply.github.com>
2024-09-26 15:33:19 +0700

    commit

--- src/main/java/com/btl/taskmanagement/App.java
+++ src/main/java/com/btl/taskmanagement/App.java
@@ -14,6 +14,7 @@ public class App extends Application {
 		ViewFactory.stage = stage;
 		ViewFactory.mainWindow = mainWindow;
 		ViewFactory.switchToMainWindow();
+		
 	}
 	
 	public static void main(String[] args) {
